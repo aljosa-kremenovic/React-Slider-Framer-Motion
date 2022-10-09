7b6f48e3f7affa759e2c3eee26af7b11bb1b871b
Aljosa Kremenovic <aljosa.kremenovic@gmail.com>
2022-10-10 01:31:33 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import { UserForm } from './components/UserForm';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import IncomeForm from './components/IncomeForm';
+import IncomeList from './components/IncomeList';
 
-const App = () => {
-  return (
-    <div className="App">
-      <UserForm />
-    </div>
-  );
+function App() {
+	const [income, setIncome] = useState([]);
+	const [totalIncome, setTotalIncome] = useState(0);
+
+	useEffect(() => {
+		let temp = 0;
+		for(let i = 0; i < income.length; i++) {
+			temp += parseInt(income[i].price);
+		}
+
+		setTotalIncome(temp);
+	}, [income]);
+	
+
+	return (
+		<div className="App">
+			<Header totalIncome={totalIncome} />
+			<IncomeForm income={income} setIncome={setIncome} />
+			<IncomeList income={income} setIncome={setIncome} />
+		</div>
+	);
 }
 
 export default App;
